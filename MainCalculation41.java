77a216f63ad297ac5e70e53c2062c619b8a62a82
amalajinu <133833157+amalajinu@users.noreply.github.com>
2023-06-09 14:43:23 +0530

    interface eg 41
    41WriteaprogramwhereRBIwill beaninterface,haveamethodrecurringDepositwhichcanaccepttheamountandduration.ThismustbeimplementedinclassHDFC

---
+++ MainCalculation41.java
@@ -0,0 +1,12 @@
+package mavenPackage;
+
+public class MainCalculation41 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		HDFC41 h=new HDFC41();
+		h.recurringDeposit(300,12);
+
+	}
+
+}
