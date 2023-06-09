53f107a63e5f8c2801edb27201da6a092145d649
amalajinu <133833157+amalajinu@users.noreply.github.com>
2023-06-09 14:40:36 +0530

    EMPLOYEE Abstract class eg
    40)	Let’s first create the superclass Employee and define a method called calculate Salary() as anabstract method. The Contractor classinherits all properties from its parent Employee but have toprovideitsownimplementationofcalculateSalary()methodandmultiplythevalueofpaymentperhour with given working hours.The full time employee has its own implementation ofcalculateSalary()method.Inthiscasewejustmultiplybyconstant 8hours

---
+++ EmpMain40.java
@@ -0,0 +1,13 @@
+package mavenPackage;
+
+public class EmpMain40 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Contractor40 c=new Contractor40();
+		c.empDetails();
+		c.salaryCal();
+
+	}
+
+}
