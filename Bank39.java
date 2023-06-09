f0026bf185de6ff753003458748f6784bfb02e4e
amalajinu <133833157+amalajinu@users.noreply.github.com>
2023-06-09 15:49:55 +0530

    encapsulation eg
    39)	ProgramtowithdrawamountfromanATM qno39

---
+++ Bank39.java
@@ -0,0 +1,22 @@
+package mavenPackage;
+
+public class Bank39 {
+	private int pin1;
+	private int pin2;
+	private int pin3;
+	private int validno;
+	public void setPin(int pin1,int pin2,int pin3)
+	{
+		this.pin1=pin1;
+		this.pin2=pin2;
+		this.pin3=pin3;
+	}
+	public void setvalid(int validno)
+	{
+		this.validno=validno;
+		System.out.println("valid no");
+	}
+	
+	
+
+}
